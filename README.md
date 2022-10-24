c42a763b78a34de9e4c5284669531bb46428c547
Rodrigo Santos <rodrigo.moh@gmail.com>
2022-10-24 10:18:12 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<div align="center" id="top">   
+
+  ![App Screenshot](img/img-21.png)
+
+  &#xa0;
+
+</div>
+
+<h1 align="center">Exercícios webpack e javascript</h1>
+
+<p align="center">
+
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/rss-ptk/aula-webpack?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/rss-ptk/aula-webpack?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/rss-ptk/aula-webpack?color=56BEB8">
+
+</p>
+
+<br>
+
+[![rss-ptk - aula-webpack](https://img.shields.io/static/v1?label=rss-ptk&message=aula-webpack&color=blue&logo=github)](https://github.com/rss-ptk/aula-webpack "Go to GitHub repo")
+[![stars - aula-webpack](https://img.shields.io/github/stars/rss-ptk/aula-webpack?style=social)](https://github.com/rss-ptk/aula-webpack)
+[![forks - aula-webpack](https://img.shields.io/github/forks/rss-ptk/aula-webpack?style=social)](https://github.com/rss-ptk/aula-webpack)
+
+## :information_source: About
+
+Esse repositório contém os arquivos dos exercícios usando a biblioteca [Moment.js](https://github.com/moment/moment/) das aulas de javascript e webpack.
+
+Essa solução também faz uso no frontend das bibliotecas [Bootstrap](https://github.com/twbs/bootstrap/) e [Inputmask](https://github.com/RobinHerbots/Inputmask/).
+
+&#xa0;
+## :rocket: Technology Stack ##
+
+The following tools were used in this project:
+
+- [node](https://github.com/nodejs/node/)
+- [webpack](https://github.com/webpack/webpack/)
+- [moment](https://github.com/moment/moment/)
+- [inputmask](https://github.com/RobinHerbots/Inputmask/)
+- [lodash](https://github.com/lodash/lodash/)
+- [bootstrap](https://github.com/twbs/bootstrap/)
+- [jquery](https://github.com/jquery/jquery/)
+
+&#xa0;
+## Autores
+
+- [@rss-ptk](https://www.github.com/rss-ptk)
+
+&#xa0;
+<a href="#top">Back to top</a>
---
+++ src/js/main.js
@@ -0,0 +1,85 @@
+import _ from 'lodash';
+import moment from 'moment';
+moment.locale('pt-br');
+
+import $ from 'jquery';
+import Inputmask from 'inputmask';
+
+import '../scss/styles.scss';
+import * as bootstrap from 'bootstrap';
+
+const time = Date();
+//console.log(time);
+
+const timeMoment = moment(time).format("h:mm:ss");
+//console.log('timeMoment', timeMoment);
+
+const botaoCalcular = document.getElementById('btnEnviar');
+botaoCalcular.addEventListener('click', calcularData);
+
+function calcularData() {
+
+    const dateTime = document.getElementById("date").value;
+
+    const resultado15 = moment(dateTime).subtract(15, 'days');
+    document.getElementById('resultado15').innerHTML = resultado15.format('dddd');
+
+    const resultado60 = moment(dateTime).subtract(60, 'days');
+    document.getElementById('resultado60').innerHTML = resultado60.format('MMMM');
+
+    const ano = moment(dateTime).year();
+
+    var natal = moment("25-12-" + ano, "DD-MM-YYYY");
+  
+    document.getElementById('resultadoAno').innerHTML = natal.format('dddd');
+    //console.log('natal:', natal);
+
+}
+
+const botaoCalcular2 = document.getElementById('btnEnviar2');
+botaoCalcular2.addEventListener('click', calcularData2);
+
+function calcularData2() {
+
+    const dateTime = document.getElementById("date2").value;
+
+    const resultado = moment(dateTime, "MM-DD-YYYY");
+    //console.log('resultado:', resultado);
+  
+    document.getElementById('resultado2').innerHTML = resultado.format('DD/MM/YYYY');    
+
+}
+
+const botaoCalcular3 = document.getElementById('btnEnviar3');
+botaoCalcular3.addEventListener('click', calcularData3);
+
+function calcularData3() {
+
+    const dateTime1 = document.getElementById("date3").value;
+    const dateTime2 = document.getElementById("date4").value;
+
+    const dt1 = moment(dateTime1);
+    const dt2 = moment(dateTime2);
+  
+    document.getElementById('resultado3').innerHTML = dt2.diff(dt1, 'days') + " dias";    
+
+}
+
+const botaoCalcular4 = document.getElementById('btnEnviar4');
+botaoCalcular4.addEventListener('click', calcularData4);
+
+function calcularData4() {
+
+    const dateTime1 = document.getElementById("date5").value;
+    const dateTime2 = document.getElementById("date6").value;
+
+    const dt1 = moment(dateTime1);
+    const dt2 = moment(dateTime2);
+  
+    document.getElementById('resultado4').innerHTML = dt2.diff(dt1, 'hours') + " horas";    
+
+}
+
+$(function() {
+    Inputmask().mask(document.querySelectorAll("input"));
+});
